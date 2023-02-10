6cca44625904268b28b40dd773bd0a1b3d171438
Aayush Rana <ayushrana38@gmail.com>
2023-02-10 14:52:20 +0530

    Created Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import Logo from "../../assets/MeTube.png";
+import Styles from "./header.module.css";
+import {
+  RiAccountCircleLine,
+  RiSearchLine,
+  RiMenuFoldFill,
+} from "react-icons/ri";
+
+export default function Header() {
+  return (
+    <>
+      <div className={Styles.navbar}>
+        <div className={Styles.siteName}>
+          <RiMenuFoldFill />
+          <img className={Styles.logo} src={Logo} alt="MeTube Logo" />
+        </div>
+        <div className={Styles.search}>
+          <input type="text" name="searchbar" placeholder="Search" />
+          <button type="submit">
+            <RiSearchLine />
+          </button>
+        </div>
+        <button type="button" className={Styles.signInBtn}>
+          <RiAccountCircleLine /> Sign In
+        </button>
+      </div>
+    </>
+  );
+}
